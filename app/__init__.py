fcf2911e2b1ac180c321e05c43dac46bb6b919a4
Lemniscate or Brainiac or itsnight.exe <Lemniscate_zero@proton.me>
2024-07-26 16:05:51 +0000

    Delete app directory
    Signed-off-by: Lemniscate or Brainiac or itsnight.exe <Lemniscate_zero@proton.me>

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from .generate_story import generate_story
-from .generate_panel import generate_panel
-from .apply_style import apply_style
-from .add_text import add_text
--- app/generate_story.py
+++
@@ -1,9 +0,0 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
-model = GPT2LMHeadModel.from_pretrained('models/text_gen_model.pth')
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-def generate_story(prompt, length=200):
-    inputs = tokenizer.encode(prompt, return_tensors='pt')
-    outputs = model.generate(inputs, max_length=length, num_return_sequences=1)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
